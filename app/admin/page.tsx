dbf9e83915981dfb651ae11b13d8f3ae79ae8234
unknown <T20S01>
2025-06-12 11:45:07 +0900

    feat: add admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>管理ページ</h1>
+      <p>ここは管理者用のページです。</p>
+      {/* ここに管理用のコンポーネントや機能を追加していきます */}
+    </div>
+  );
+};
+
+export default AdminPage;
